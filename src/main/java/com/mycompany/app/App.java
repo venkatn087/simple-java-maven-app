6475aeb3c694f6859f602acda0e49b2474b8bb5c
venkatn087 <52975559+venkatn087@users.noreply.github.com>
2023-09-02 12:15:36 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,12 +11,14 @@ public class App {
   // var lml;
   // var hetes;
   // var tes;
+  //var hellopoooo
   private final String message = "Hello World!";
 
   public App() {}
 
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
+    //System.out.println(new App().getMessage());
   }
 
   private final String getMessage() {
