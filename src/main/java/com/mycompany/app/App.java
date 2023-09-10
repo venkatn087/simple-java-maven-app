f9c43a3f2bf3955fe8caec3425bc2a7aac19c027
sudharshan078 <nelaturisudharshan8@gmail.com>
2023-09-10 12:42:51 +0530

    added spotbugs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,14 +11,15 @@ public class App {
   // var lml;
   // var hetes;
   // var tes;
-  //var hellopoooo
+  // var hellopoooo
+  private int abc;
   private final String message = "Hello World!";
 
   public App() {}
 
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
-    //System.out.println(new App().getMessage());
+    // System.out.println(new App().getMessage());
   }
 
   private final String getMessage() {
